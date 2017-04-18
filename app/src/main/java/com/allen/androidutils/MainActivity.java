89ec4f17af28722dc057b3289626c75f776c3bf2
Allen <lygttpod@163.com>
2017-04-18 16:08:32 +0800

    新增权限管理基类

--- app/src/main/java/com/allen/androidutils/MainActivity.java
+++ app/src/main/java/com/allen/androidutils/MainActivity.java
@@ -1,13 +1,41 @@
 package com.allen.androidutils;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.allen.utils.ToastUtils;
+import com.allen.utils.permission.BasePermissionActivity;
+import com.allen.utils.permission.PermissionListener;
+
+import java.util.List;
+
+public class MainActivity extends BasePermissionActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestRuntimePermission(new String[]{Manifest.permission.CALL_PHONE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.ACCESS_FINE_LOCATION},
+                new PermissionListener() {
+                    @Override
+                    public void onGranted() {
+                        ToastUtils.showShort(MainActivity.this, "应用已授权！");
+                    }
+
+                    @Override
+                    public void onDenied(List<String> deniedPermission) {
+                        for (String permission :
+                                deniedPermission) {
+                            Toast.makeText(MainActivity.this, "申请权限" + permission + "被拒绝！",Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
     }
 }
--- library/src/main/java/com/allen/utils/ActivityCollector.java
+++ library/src/main/java/com/allen/utils/ActivityCollector.java
@@ -11,14 +11,25 @@ import java.util.List;
  */
 
 public class ActivityCollector {
+
     public static List<Activity> activities = new ArrayList<Activity>();
 
     public static void addActivity(Activity activity) {
         activities.add(activity);
     }
+
     public static void removeActivity(Activity activity) {
         activities.remove(activity);
     }
+
+    public static Activity getTopActivity() {
+        if (activities.isEmpty()) {
+            return null;
+        } else {
+            return activities.get(activities.size() - 1);
+        }
+    }
+
     public static void finishAll() {
         for (Activity activity : activities) {
             if (!activity.isFinishing()) {
